20741a169e90283ea1e3d73fd0f1a633b4548a7b
James Bowers <jamesbowers123@gmail.com>
2022-03-07 20:41:57 +0000

    add drum kit project

---
+++ projects/JavaScript/Drum-Kit/index.js
@@ -0,0 +1,62 @@
+
+// Detecting the Button Press
+
+// Loop to assign an Event Listener (for mouse clicks!) to each item with the class 'drum'
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+
+document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        
+    let buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    
+});
+
+
+// Detecting keyboard press
+
+document.addEventListener("keydown", function (event) {
+    makeSound(event.key);
+});
+
+function makeSound(key) {
+    
+    switch (key) {
+        case "w":
+            let kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+
+        case "a":
+            let snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+    
+        case "s":
+            let tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "d":
+            let tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "j":
+            let tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "k":
+            let tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case "l":
+            let crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+
+            default: console.log(buttonInnerHTML);
+        }
+    };
+}
